399af16abd94c7bf2eec7e23574ea2e10ed86589
beekock <solovequa@gmail.com>
2022-10-22 23:26:06 +0300

    personalPage created

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import styles from "./App.module.scss";
 import { LobbyPage } from "./pages/LobbyPage";
+import PersonalPage from "../components/PersonalPage";
 
 function App() {
   return (
     <div className={styles.app}>
       <LobbyPage className="page" />
+      <div className="page__personal">
+        <PersonalPage/>
+      </div>
     </div>
   );
 }
